fb2d7525fe52741a0b1f860e4ff91ce45074de1b
doppelgangger11 <gamenightmer@gmail.com>
2024-11-07 14:43:22 +0500

    initial commit

---
+++ AITrainer.py
@@ -0,0 +1,73 @@
+# TODO see in todo.md
+
+import cv2
+import numpy as np
+import time
+from squats import pose_module1 as pm
+
+# * change the destination to file for testing if you have another name or directory
+cap = cv2.VideoCapture("./data/train/чел приседает.mp4")
+
+# video_mode: str = input("Will you use video (y/n): ")
+
+# if video_mode == "y":
+#     cap = cv2.VideoCapture("C:/Users/user/Desktop/чел приседает.mp4")
+# else:
+#     pass
+# cap = cv2.VideoCapture(0)
+
+detector = pm.poseDetector()
+count = 0
+direction = 0
+# previous time - надо для расчета fps наху
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (1280,720))
+    
+    img = detector.findPose(img, False)
+    lmList = detector.findPosition(img, False)
+    #print(lmList)
+    if len(lmList) != 0:
+        angle = detector.findAngle(img, 24, 26, 28)
+        #angle1 = detector.findAngle(img, 23, 25, 27)
+
+        # Переводим углы в проценты, где минимальный угол в присяде равен 0%, а максимальный - 100%
+        per = np.interp(angle, (68,171), (0,100))
+        #print(angle, per)
+
+        bar = np.interp(angle, (68,171), (650, 100))
+
+        # Делаем наш счетчик 
+        if per == 0:
+            if direction == 0:
+                count += 0.5
+                direction = 1
+        if per == 100:
+            if direction == 1:
+                count += 0.5
+                direction = 0
+        print(count)
+
+        cv2.rectangle(img, (1100,100), (1175, 650), (34,255,0), 3)
+        cv2.rectangle(img, (1100,int(bar)), (1175, 650), (34,255,0), cv2.FILLED)
+        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4, (255,0,0), 4)
+
+
+        cv2.rectangle(img, (0,450), (250, 720), (34,255,0), cv2.FILLED)
+        cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15, (255,0,0), 25)
+
+
+
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255,0,0), 5)
+
+
+        if count > 20:
+            break
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
---
+++ squats/pose_module1.py
@@ -0,0 +1,117 @@
+import cv2
+import mediapipe as mp # type: ignore
+import time
+import math
+
+class poseDetector:
+    def __init__(self, mode=False, model_complexity=1, enable_segmentation=False, 
+                 smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.model_complexity = model_complexity
+        self.enable_segmentation = enable_segmentation
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(
+            static_image_mode=self.mode, 
+            model_complexity=self.model_complexity, 
+            enable_segmentation=self.enable_segmentation,
+            smooth_landmarks=self.smooth,
+            min_detection_confidence=self.detectionCon, 
+            min_tracking_confidence=self.trackCon
+        )
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
+    
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 4, (255, 0, 0), cv2.FILLED)
+        return self.lmList
+    
+    def findAngle(self, img, p1, p2, p3, draw = True):
+
+        # Получаем наши координаты
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Вычисляем угол
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+        #print(angle)
+
+        if angle < 0:
+            angle += 360
+
+        # Рисуем наши линии и круглишочки, чтобы визуально было понятно, че к чему
+        if draw:
+            cv2.line(img, (x1,y1), (x2,y2), (255,255,255), 3)
+            cv2.line(img, (x3,y3), (x2,y2), (255,255,255), 3)
+            cv2.circle(img, (x1, y1), 8, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 14, (255, 0, 0), 2)
+            cv2.circle(img, (x2, y2), 8, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 14, (255, 0, 0), 2)
+            cv2.circle(img, (x3, y3), 8, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 14, (255, 0, 0), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
+
+
+        return angle
+
+
+def main():
+    cap = cv2.VideoCapture("../data/train/чел приседает.mp4")
+    pTime = 0  # Previous time
+    scale_factor = 0.7  # Масштабирование кадра, можно изменить для лучшего вписывания
+
+    detector = poseDetector()
+    
+    while True:
+        success, img = cap.read()
+        if not success:
+            break  # Остановить, если видео закончилось
+
+        # Изменение размера кадра
+        height, width = img.shape[:2]
+        img_resized = cv2.resize(img, (int(width * scale_factor), int(height * scale_factor)))
+
+        img_resized = detector.findPose(img_resized)
+        lmList = detector.findPosition(img_resized, draw=False)
+
+        if len(lmList) != 0:
+            for idx in [25, 26]:  # Индексы элементов, которые нужно обработать
+                print(lmList[idx])
+                cv2.circle(img_resized, (lmList[idx][1], lmList[idx][2]), 15, (0, 0, 255), cv2.FILLED)
+        
+        # Расчет FPS
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img_resized, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        # Отображение результата
+        cv2.imshow("Resized Image", img_resized)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
